b077430ed34aac5860c62be9f4525a758e1d16d1
github.io/codecole <cole.iceneil@gmail.com>
2019-11-24 20:57:05 +0100

    Update to Callback and CSV

--- callback.php
+++ callback.php
@@ -5,7 +5,7 @@
 $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
-$text        = $_GET["text"];
+$text        = $_POST["text"];
 
 
 //This is the first menu 

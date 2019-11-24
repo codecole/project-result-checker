09390e9800fe249e22a15804f99351a8f0f63375
github.io/codecole <cole.iceneil@gmail.com>
2019-11-24 20:18:07 +0100

    Updated Calback

--- callback.php
+++ callback.php
@@ -11,6 +11,8 @@ $text        = $_POST["text"];
 //This is the first menu 
 if ( $text == "" ) {
     $response  = "CON Welcome, To check your result enter your matric number below\n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
 
 }
 
@@ -19,21 +21,32 @@ if ( $text == "" ) {
 
 // Menu for a user who selects '1'
 
-if($text=="22"){
-    // User has entered a matric number
+// if($text=="22"){
+//     // User has entered a matric number
 
-    // Check if matric number is valid
+//     // Check if matric number is valid
 
-    // Get student result usng matric number from database
+//     // Get student result usng matric number from database
 
-    // 
-    $response="Hi Cole, Your result is: ENG:A1,MTH:B2 \n";
+//     // 
+//     $response="Hi Cole, Your result is: ENG:A1,MTH:B2 \n";
 
+// }
+
+else if ($text == "1") {
+    $response  = "CON Choose account information you want to view \n";
+    $response .= "1. Account Number \n";
+    $response .= "2. Account Balance \n";
 }
+
 
 
 //Menu for a user who selects '2' 
 
+else if ($text == "2") {
+    //note that we are using the $phoneNumber variable we got form the HTTP POST data.
+    $response = "END Your phone number is $phoneNumber\n";
+}
 
 
 
@@ -41,11 +54,17 @@ if($text=="22"){
 //Menu for a user who selects '1' from second menu
 
 
+else if ($text == "1*1") {
+    $response = "END Your account number is ACC1001\n";
+}
 
 
 
 //Menu for a user who selects '2' from second menu
 
+else if ($text == "1*2") {
+    $response = "END Your balance is USD 10.78\n";
+}
 
 
 
